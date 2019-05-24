ee1f958b25e0a8a0b422ea44a701484313f45672
Stefan <bogosavljevic.1911@gmail.com>
2019-05-24 18:00:12 +0200

    Segrt Mapiranje

---
+++ SBP_Projekat/SBP_Project_data/Mapiranja/SegrtMapiranje.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SBP_Project_data.Models;
+using FluentNHibernate.Mapping;
+
+namespace SBP_Project_data.Mapiranja
+{
+    class SegrtMapiranje : ClassMap<Segrt>
+    {
+        public SegrtMapiranje()
+        {
+            Table("Segrt");
+            Id(x => x.Id, "SEGRT_ID").GeneratedBy.SequenceIdentity("S16022.RASA_ID_SEQ");
+            Map(x => x.Ime).Column("IME");
+            Map(x => x.Bonus).Column("BONUS");
+
+        }
+    }
+}
