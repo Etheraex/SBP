1cfd52fd7f0bc8a0e7049fa1a2ad63c485bd0861
Stefan <bogosavljevic.1911@gmail.com>
2019-06-13 15:40:14 +0200

    Alijansa data grid

--- SBP_Projekat/SBP_FormApp/Forme/AlianceForm.cs
+++ SBP_Projekat/SBP_FormApp/Forme/AlianceForm.cs
@@ -1,4 +1,6 @@
 ﻿using SBP_Data.DTOs;
+using SBP_Data;
+using SBP_Data.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +15,30 @@ namespace SBP_Projekat.Forme
 {
     public partial class AlianceForm : Form
     {
+        IgracDTO _igrac;
         public AlianceForm(IgracDTO igrac,Form parent)
         {
             InitializeComponent();
             this.MdiParent = parent;
+            _igrac = igrac;
+           
         }
+
+        private void AlianceForm_Load(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+               
+                   
+                dgvAlijanse.Invoke((MethodInvoker)(() =>
+                {
+                    dgvAlijanse.DataSource = DTOManager.Instance.getDTOList<AlijansaDTO, Alijansa>();
+                }));
+            }
+            );
+
+        }
+
+        
     }
 }
