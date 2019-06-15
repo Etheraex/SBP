c1785010855163869b3cad84aa6fca6fc0209a43
Jakov Barton <jakov.barton@elfak.rs>
2019-06-15 14:57:49 +0200

    AddItem&newRaseForm

---
+++ SBP_Projekat/SBP_FormApp/Forme/AddItemForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SBP_Projekat.Forme
+{
+    partial class AddItemForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // AddItemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 237);
+            this.Name = "AddItemForm";
+            this.Text = "AddItem";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
